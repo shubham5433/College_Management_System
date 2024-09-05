b2c3891c558a6d22dacd7170ee6b62f6f270ea7e
shubham5433 <shubhamsingh5433@gmail.com>
2024-09-05 20:09:37 +0530

    Initial commit - Upload MERN project

---
+++ frontend/src/components/buttonStyles.js
@@ -0,0 +1,104 @@
+import styled from 'styled-components';
+import { Button } from '@mui/material';
+
+export const RedButton = styled(Button)`
+  && {
+    background-color: black;
+    color: #fff;
+    margin-left: 4px;
+    &:hover {
+      background-color: white;
+      color:black;
+    }
+  }
+`;
+
+export const BlackButton = styled(Button)`
+ 
+  && {
+    background-color: black;
+    color: #fff;
+    margin-left: 4px;
+    &:hover {
+      background-color: white;
+      color:black;
+    }
+  }
+`;
+
+export const DarkRedButton = styled(Button)`
+&& {
+  background-color: black;
+  color: #fff;
+  &:hover {
+    background-color: white;
+    color:black;
+  }
+}
+`;
+
+export const BlueButton = styled(Button)`
+&& {
+  background-color: black;
+  color: #fff;
+  &:hover {
+    background-color: white;
+    color:black;
+  }
+}
+`;
+
+export const PurpleButton = styled(Button)`
+&& {
+  background-color: black;
+  color: #fff;
+  &:hover {
+    background-color: white;
+    color:black;
+  }
+}
+`;
+
+export const LightPurpleButton = styled(Button)`
+  && {
+    background-color: black;
+    color: #fff;
+    &:hover {
+      background-color: white;
+      color:black;
+    }
+  }
+`;
+
+export const GreenButton = styled(Button)`
+&& {
+  background-color: black;
+  color: #fff;
+  &:hover {
+    background-color: white;
+    color:black;
+  }
+}
+`;
+
+export const BrownButton = styled(Button)`
+&& {
+  background-color: black;
+  color: #fff;
+  &:hover {
+    background-color: white;
+    color:black;
+  }
+}
+`;
+
+export const IndigoButton = styled(Button)`
+&& {
+  background-color: black;
+  color: #fff;
+  &:hover {
+    background-color: white;
+    color:black;
+  }
+}
+`;
